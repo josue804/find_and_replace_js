52a5beb1e4a287cd78986dc4ac57b7b418df0fde
Josue Valverde <student@epicodus.com>
2015-09-11 09:51:11 -0700

    add code to fetch user input and run function, returning correctly modified string.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
 var findAndReplace = function(phrase, wordToFind, replacementWord) {
   return phrase.replace(wordToFind, replacementWord);
 }
+
+$(function() {
+  $("form#find-and-replace").submit(function(event) {
+    var phrase = $("input#phrase").val();
+    var wordToFind = $("input#wordToFind").val();
+    var replacementWord = $("input#replacementWord").val();
+    var result = findAndReplace(phrase, wordToFind, replacementWord);
+    $(".resultingString").text(result);
+
+    $("#result").show();
+    event.preventDefault();
+  });
+});
